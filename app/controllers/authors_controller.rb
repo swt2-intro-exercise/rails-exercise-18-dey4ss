684bf765d4559c9dc25c3ebf90dba0f850c7a3f5
Daniel Lindner <lindnerdaniel@yahoo.de>
2018-10-26 16:56:59 +0200

    add failing test for #6

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,8 @@
 class AuthorsController < ApplicationController
+
+    def create
+        @author = Author.new(params[:first_name, :last_name, :homepage])
+        @author.save
+        redirect_to @author
+    end
 end
