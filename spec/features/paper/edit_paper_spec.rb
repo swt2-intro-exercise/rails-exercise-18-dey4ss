06102efb7ac5c2f5f36cbc1226ad9bb3c6140081
Daniel Lindner <lindnerdaniel@yahoo.de>
2018-10-27 19:02:26 +0200

    update paper's authors #21

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -12,15 +12,15 @@ describe "Edit paper page", type: :feature do
         expect(page).to have_css("select[multiple]")
     end
 
-    #it "shuld update authors of a paper" do
-    #    authors = @paper.authors.count
-    #    visit edit_paper_path(@paper)
-    #    puts page.html
-    #    page.select  @author.name, from: "paper[author_ids][]"
-    #    find('input[type="submit"]').click
+    it "shuld update authors of a paper" do
+        authors = @paper.authors.count
+        visit edit_paper_path(@paper)
+        #puts page.html
+        page.select  @author.name, from: "paper[author_ids][]"
+        find('input[type="submit"]').click
 
-    #    @paper.reload
-        #expect(@paper.authors.count).to eq (authors + 1)
-    #    expect(@paper.authors).to include(@author)
-    #end
+        @paper.reload
+        expect(@paper.authors.count).to eq (authors + 1)
+        expect(@paper.authors).to include(@author)
+    end
 end
