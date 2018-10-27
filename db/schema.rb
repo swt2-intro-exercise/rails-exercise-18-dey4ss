0d9d39ea7e3c9dccf05c79b7be0d8131cd43dd5e
Daniel Lindner <lindnerdaniel@yahoo.de>
2018-10-27 17:06:09 +0200

    add papers #16

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_155243) do
+ActiveRecord::Schema.define(version: 2018_10_27_145519) do
 
   create_table "authors", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_10_26_155243) do
     t.text "first_name"
     t.text "last_name"
     t.text "homepage"
+  end
+
+  create_table "papers", force: :cascade do |t|
+    t.string "title"
+    t.string "venue"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
